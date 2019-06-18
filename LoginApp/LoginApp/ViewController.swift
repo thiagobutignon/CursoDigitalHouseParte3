63417b21e4a78f96fd4500383844f83d71257912
Thiago Butignon <thiagobutignon@gmail.com>
2019-06-18 19:54:45 -0300

    criado o projeto, instalado o pod textfield effects

---
+++ LoginApp/LoginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Thiago B Claramunt on 18/06/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
