95ebd9269ea99f77b9062e68dfdee81e58bae388
Thiago Butignon <thiagobutignon@gmail.com>
2019-06-24 20:40:22 -0300

    projeto adicionado

--- LoginApp/LoginApp/ViewController.swift
+++ LoginApp/LoginApp/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import PMSuperButton
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: PMSuperButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func teste(_ sender: PMSuperButton) {
+        button.showLoader(userInteraction: true)
+        
+        print("Funciona")
+        
+        
+    }
+    
 }
 
