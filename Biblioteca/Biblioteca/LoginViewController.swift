b59db58c0e5d9305882f0274a1e0d867dc08ffc7
Thiago Butignon <thiagobutignon@gmail.com>
2019-06-25 17:14:43 -0300

    tela de boas vindas conclúida. Perform segue para a tela de lista de livros e alerta para caso o usuário não preencha o usuário

---
+++ Biblioteca/Biblioteca/LoginViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Biblioteca
+//
+//  Created by Thiago B Claramunt on 25/06/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    let usernameAlert = UIAlertController(title: "Usuário não encontrado", message: "Coloque um nome de usuário válido", preferredStyle: .alert)
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        if usernameTextField.hasText == true {
+            performSegue(withIdentifier: "loggedIn", sender: nil)
+        } else {
+            usernameAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(usernameAlert, animated: true, completion: nil)
+        }
+    }
+}
