62f0b0128c5dee7231c903646004be61e51195c7
Thiago Butignon <thiagobutignon@gmail.com>
2019-06-27 14:25:59 -0300

    terminado de implementar o ScrollViewController

--- Biblioteca/Biblioteca/View/MainViewController.swift
+++ Biblioteca/Biblioteca/View/MainViewController.swift
@@ -27,10 +27,28 @@ class MainViewController: UIViewController {
     var listBooksViewController: ListBooksViewController!
     
     @IBOutlet var navigationView: NavigationView!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let controller = segue.destination as? ListBooksViewController {
+            listBooksViewController = controller
+        } else if let controller = segue.destination as? ScrollViewController {
+            scrollViewController = controller
+            scrollViewController.delegate = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
     }
 
+}
+
+extension MainViewController {
+    @IBAction func handleAddBookIconTap(_ sender: UITapGestureRecognizer) {
+        scrollViewController.setController(to: addBookViewController, animated: true)
+    }
+    
+    @IBAction func handleStatisticIconTap(_ sender: UITapGestureRecognizer) {
+        scrollViewController.setController(to: statisticViewController, animated: true)
+    }
 }
