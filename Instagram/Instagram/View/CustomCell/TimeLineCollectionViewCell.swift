25cb7aa70e7cf677341147c8685b89f0a2a9b036
Thiago Butignon <thiagobutignon@gmail.com>
2019-07-02 19:47:19 -0300

    reorganizada as pastas e criado a collection custom cell

---
+++ Instagram/Instagram/View/CustomCell/TimeLineCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TimeLineCollectionViewCell.swift
+//  Instagram
+//
+//  Created by Thiago B Claramunt on 02/07/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
